6371cc426d9ee28b61dc33279d9a922d8d5d2c5c
Jens Nyman <jnyman@google.com>
2022-10-26 07:45:38 +0000

    Add a Kotlin test to supplement the Java/JVM and Java/Android tests.
    This test will be used to test whether https://github.com/google/TestParameterInjector/issues/22 is fixed

---
+++ junit4/src/test/java/com/google/testing/junit/testparameterinjector/TestParameterInjectorKotlinTest.kt
@@ -0,0 +1,188 @@
+/*
+ * Copyright 2022 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.testing.junit.testparameterinjector
+
+import com.google.common.collect.ImmutableList
+import com.google.common.collect.ImmutableMap
+import com.google.testing.junit.testparameterinjector.SharedTestUtilitiesJUnit4.SuccessfulTestCaseBase
+import java.util.Arrays
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import org.junit.runners.Parameterized.Parameters
+
+@RunWith(Parameterized::class)
+class TestParameterInjectorKotlinTest {
+
+  // ********** Test cases ********** //
+  // These test classes are all expected to run successfully
+
+  @RunAsTest
+  internal class TestParameter_MethodParam : SuccessfulTestCaseBase() {
+    @Test
+    fun testString(@TestParameter("a", "b") param: String) {
+      storeTestParametersForThisTest(param)
+    }
+
+    @Test
+    fun testEnum_selection(@TestParameter("RED", "GREEN") param: Color) {
+      storeTestParametersForThisTest(param)
+    }
+
+    @Test
+    fun testEnum_all(@TestParameter param: Color) {
+      storeTestParametersForThisTest(param)
+    }
+
+    @Test
+    fun testMultiple(
+      @TestParameter("1", "8") width: Int,
+      @TestParameter("1", "5.5") height: Double,
+    ) {
+      storeTestParametersForThisTest(width, height)
+    }
+
+    override fun expectedTestNameToStringifiedParameters(): ImmutableMap<String, String> {
+      return ImmutableMap.builder<String, String>()
+        .put("testString[a]", "a")
+        .put("testString[b]", "b")
+        .put("testEnum_selection[RED]", "RED")
+        .put("testEnum_selection[GREEN]", "GREEN")
+        .put("testEnum_all[RED]", "RED")
+        .put("testEnum_all[BLUE]", "BLUE")
+        .put("testEnum_all[GREEN]", "GREEN")
+        .put("testMultiple[width=1,height=1.0]", "1:1.0")
+        .put("testMultiple[width=1,height=5.5]", "1:5.5")
+        .put("testMultiple[width=8,height=1.0]", "8:1.0")
+        .put("testMultiple[width=8,height=5.5]", "8:5.5")
+        .buildOrThrow()
+    }
+  }
+
+  @RunAsTest
+  internal class TestParameter_Field : SuccessfulTestCaseBase() {
+    @TestParameter("1", "2") var width: Int? = null
+
+    @Test
+    fun test() {
+      storeTestParametersForThisTest(width)
+    }
+
+    override fun expectedTestNameToStringifiedParameters(): ImmutableMap<String, String> {
+      return ImmutableMap.builder<String, String>()
+        .put("test[width=1]", "1")
+        .put("test[width=2]", "2")
+        .buildOrThrow()
+    }
+  }
+
+  @RunAsTest
+  internal class TestParameter_ConstructorParam : SuccessfulTestCaseBase {
+    val width: Int
+
+    constructor(@TestParameter("1", "2") width: Int) {
+      this.width = width
+    }
+
+    @Test
+    fun test() {
+      storeTestParametersForThisTest(width)
+    }
+
+    override fun expectedTestNameToStringifiedParameters(): ImmutableMap<String, String> {
+      return ImmutableMap.builder<String, String>()
+        .put("test[width=1]", "1")
+        .put("test[width=2]", "2")
+        .buildOrThrow()
+    }
+  }
+
+  @RunAsTest
+  internal class TestParameters_MethodParam : SuccessfulTestCaseBase() {
+    @TestParameters("{width: 3, height: 8}")
+    @TestParameters("{width: 5, height: 2.5}")
+    @Test
+    fun test(width: Int, height: Double) {
+      storeTestParametersForThisTest(width, height)
+    }
+
+    override fun expectedTestNameToStringifiedParameters(): ImmutableMap<String, String> {
+      return ImmutableMap.builder<String, String>()
+        .put("test[{width: 3, height: 8}]", "3:8.0")
+        .put("test[{width: 5, height: 2.5}]", "5:2.5")
+        .buildOrThrow()
+    }
+  }
+
+  @RunAsTest
+  internal class TestParameters_ConstructorParam : SuccessfulTestCaseBase {
+    val width: Int
+
+    @TestParameters("{width: 1}")
+    @TestParameters("{width: 2}")
+    constructor(width: Int) {
+      this.width = width
+    }
+
+    @Test
+    fun test() {
+      storeTestParametersForThisTest(width)
+    }
+
+    override fun expectedTestNameToStringifiedParameters(): ImmutableMap<String, String> {
+      return ImmutableMap.builder<String, String>()
+        .put("test[{width: 1}]", "1")
+        .put("test[{width: 2}]", "2")
+        .buildOrThrow()
+    }
+  }
+
+  // ********** Test infrastructure ********** //
+
+  private val testClass: Class<*>
+
+  constructor(@Suppress("UNUSED_PARAMETER") testName: String?, testClass: Class<*>) {
+    this.testClass = testClass
+  }
+
+  @Test
+  fun test_success() {
+    SharedTestUtilitiesJUnit4.runTestsAndAssertNoFailures(
+      object : PluggableTestRunner(testClass) {
+        override fun createTestMethodProcessorList(): TestMethodProcessorList {
+          return TestMethodProcessorList.createNewParameterizedProcessors()
+        }
+      }
+    )
+  }
+
+  companion object {
+    @JvmStatic
+    @Parameters(name = "{0}")
+    fun parameters(): Collection<Array<Any>> {
+      return Arrays.stream(TestParameterInjectorKotlinTest::class.java.classes)
+        .filter { cls: Class<*> -> cls.isAnnotationPresent(RunAsTest::class.java) }
+        .map { cls: Class<*> -> arrayOf<Any>(cls.simpleName, cls) }
+        .collect(ImmutableList.toImmutableList())
+    }
+  }
+  annotation class RunAsTest
+
+  enum class Color {
+    RED,
+    BLUE,
+    GREEN
+  }
+}
