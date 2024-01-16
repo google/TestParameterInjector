c46648271d3e27e6311e708ee8278186d4e7d361
Jens Nyman <jnyman@google.com>
2024-01-16 13:55:12 +0000

    Make TestParameterValuesProvider.provideValues() protected so it can actually be used by clients.
    https://github.com/google/TestParameterInjector/issues/44

--- junit4/src/main/java/com/google/testing/junit/testparameterinjector/TestParameterValuesProvider.java
+++ junit4/src/main/java/com/google/testing/junit/testparameterinjector/TestParameterValuesProvider.java
@@ -32,7 +32,7 @@ import javax.annotation.Nullable;
 public abstract class TestParameterValuesProvider
     implements TestParameter.TestParameterValuesProvider {
 
-  abstract List<?> provideValues(Context context);
+  protected abstract List<?> provideValues(Context context);
 
   @Override
   public final List<?> provideValues() {
