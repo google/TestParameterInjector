88d9bf602972c15a1729658943cf078510bf9f3a
nymanjens <nymanjens.nj@gmail.com>
2025-09-09 14:17:25 +0000

    Deploying to site from @ google/TestParameterInjector@dee42b7d02830d906cea41649f5066bdbce42eec 🚀

--- docs/latest/package-search-index.js
+++ docs/latest/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.google.testing.junit.testparameterinjector"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.google.testing.junit.testparameterinjector"}];updateSearchResults();
